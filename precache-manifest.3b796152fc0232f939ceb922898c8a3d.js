4882ff6ad392c8838b1891cb6a61e94394857917
karan.s1 <karan.s1@naukri.com>
2020-07-30 06:42:55 +0530

    Updates

---
+++ precache-manifest.3b796152fc0232f939ceb922898c8a3d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14b2f01509d4946f6127605ac2d79ace",
+    "url": "/fbDropdown/index.html"
+  },
+  {
+    "revision": "c9d55a3acdbca25122d3",
+    "url": "/fbDropdown/static/css/main.1d36f2ef.chunk.css"
+  },
+  {
+    "revision": "938cd96690ebb46cc269",
+    "url": "/fbDropdown/static/js/2.f0be0bb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fbDropdown/static/js/2.f0be0bb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d55a3acdbca25122d3",
+    "url": "/fbDropdown/static/js/main.cd7b8abb.chunk.js"
+  },
+  {
+    "revision": "2da517758f0baea640ce",
+    "url": "/fbDropdown/static/js/runtime-main.3e04f92f.js"
+  },
+  {
+    "revision": "48ae6434de48325549277b49f7ebf731",
+    "url": "/fbDropdown/static/media/arrow.48ae6434.svg"
+  },
+  {
+    "revision": "e9fd40a70989ed93a960628beaf25b7d",
+    "url": "/fbDropdown/static/media/bell.e9fd40a7.svg"
+  },
+  {
+    "revision": "615e14d18e96dc6cb0e028991a60ef80",
+    "url": "/fbDropdown/static/media/caret.615e14d1.svg"
+  },
+  {
+    "revision": "efbb2bd6e6e3066e53de373d26c1e039",
+    "url": "/fbDropdown/static/media/chevron.efbb2bd6.svg"
+  },
+  {
+    "revision": "993f0cc0e8de3fe0490e28fad0ee23a3",
+    "url": "/fbDropdown/static/media/cog.993f0cc0.svg"
+  },
+  {
+    "revision": "9fdc8d67348585808e061c4755bb68f9",
+    "url": "/fbDropdown/static/media/messenger.9fdc8d67.svg"
+  },
+  {
+    "revision": "9311af2e3926a973133dc6ebe8e8a9aa",
+    "url": "/fbDropdown/static/media/plus.9311af2e.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/fb-dd/precache-manifest.9e4f68d6150ace19f58ef9ff8f9f942e.js"
+  "/fbDropdown/precache-manifest.3b796152fc0232f939ceb922898c8a3d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fb-dd/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fbDropdown/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
